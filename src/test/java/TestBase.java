736f6db62a4327cd2818191634b3c7e1487ac768
Alex Ov <alexov@swisscows.email>
2024-10-14 22:34:11 +0300

    Add TestBase with AllureSelenide

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,11 @@
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+
+    @BeforeAll
+    static void beforeAll() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+}
