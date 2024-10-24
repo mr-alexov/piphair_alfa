6489858777693453d12861f268fdc21d94b1c256
Alex Ov <alexov@swisscows.email>
2024-10-24 18:52:08 +0300

    Добавляем капабилити для того чтобы на Селеноиде записывалось видео.

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,6 +6,9 @@ import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.sleep;
 
@@ -19,6 +22,14 @@ public class TestBase {
 
         Configuration.browserSize = "1920x1080";
         Configuration.timeout = 7000;
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
         sleep(1000);
 
     }
